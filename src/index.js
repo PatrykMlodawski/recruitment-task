ec2000d43aeed162e066e670ffb98b9985a6e527
PatrykMlodawski <pk.mlodawski@gmail.com>
2021-04-09 23:13:51 +0200

    Added popup creating and api handler

--- src/index.js
+++ src/index.js
@@ -1,13 +1,58 @@
 import createPopup from './popup';
 import getVendors from './apiHandler';
 
-const test = document.createElement('div');
-test.textContent = 'test';
-const popup = createPopup('GDPR consent', test);
+const vendorsList = document.createElement('ul');
+vendorsList.classList.add('gdpr-popup-list');
 
-let data;
-(async () => {
-  data = await getVendors();
-  console.log(data);
-  document.body.appendChild(popup);
-})();
+const vendors = {
+  names: [],
+  policyUrls: [],
+};
+
+function createListItem(name, policyUrl) {
+  const item = document.createElement('li');
+  const input = document.createElement('input');
+  input.type = 'checkbox';
+  input.id = name;
+  const label = document.createElement('label');
+  label.htmlFor = name;
+  const paragraphUrl = document.createElement('p');
+  paragraphUrl.classList.add('list-item-url');
+  paragraphUrl.textContent = `${name}, Politics of privacy: `;
+  const url = document.createElement('a');
+  url.textContent = policyUrl;
+  url.href = policyUrl;
+
+  paragraphUrl.appendChild(url);
+  label.appendChild(paragraphUrl);
+  item.appendChild(input);
+  item.appendChild(label);
+
+  return item;
+}
+
+function setVendors() {
+  (async () => {
+    const data = Object.values(await getVendors());
+    data.forEach((vendor) => {
+      vendors.names.push(vendor.name);
+      vendors.policyUrls.push(vendor.policyUrl);
+    });
+
+    for (let i = 0; i < vendors.names.length; i++) {
+      vendorsList.appendChild(createListItem(vendors.names[i], vendors.policyUrls[i]));
+    }
+
+    const popup = createPopup('GDPR consent', vendorsList);
+
+    console.log(data);
+    console.log(vendors);
+    document.body.appendChild(popup);
+  })();
+}
+
+function init() {
+  setVendors();
+}
+
+init();
